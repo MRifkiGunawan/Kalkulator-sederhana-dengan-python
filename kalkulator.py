f1a3a58a5480b0b783cc6b8226510b308805ec6f
MRifkiGunawan <126083394+MRifkiGunawan@users.noreply.github.com>
2023-08-30 20:02:14 +0700

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,17 @@
+operator = input("masukan operator (+,-,/,*)")
+angka1 = int(input("masukan bilangan pertama"))
+angka2 = int(input("masukan bilangan ke dua"))
+if operator == "+":{
+    print(angka1+angka2)
+}
+if operator == "-":{
+    print(angka1-angka2)
+}
+if operator == "/":{
+    print(angka1/angka2)
+}
+if operator == "*":{
+    print(angka1*angka2)
+}
+
+
